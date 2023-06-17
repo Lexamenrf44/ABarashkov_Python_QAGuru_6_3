2acc6e57e888821b029b84469046b6247b877f91
Alexander <94826692+Lexamenrf44@users.noreply.github.com>
2023-06-17 15:03:57 +0400

    Conflict (#1)

--- main.py
+++ main.py
@@ -7,5 +7,6 @@
 def print_hi(name):
     # Use this conflict instead
     # Use a breakpoint in the code line below to debug your script.
-
+    
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    
